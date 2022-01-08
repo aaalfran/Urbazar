859e32d150f2bead86f07d4c7a52aa98eac95948
Betsii77 <belinaza@espol.edu.ec>
2022-01-08 17:04:48 -0500

    Test

--- front-administracion/src/Components/Admin_Etapas/PanelEtapasV.js
+++ front-administracion/src/Components/Admin_Etapas/PanelEtapasV.js
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import { Input } from 'reactstrap';
-import AddIcon from '@material-ui/icons/Add';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
-import CreateIcon from '@material-ui/icons/Create';
-import '../../css/PanelEtapas.css';
-import LoadEtapas from "./LoadEtapas";
-import { Redirect} from 'react-router-dom';
-=======
 import React from 'react'
 import { Input } from 'reactstrap'
 import AddIcon from '@material-ui/icons/Add'
@@ -34,7 +15,6 @@ import CreateIcon from '@material-ui/icons/Create'
 import '../../css/PanelEtapas.css'
 import LoadEtapas from './LoadEtapas'
 import { Redirect } from 'react-router-dom'
->>>>>>> 7a130e371e37b75785df5c5f395eb6b061d750e9
 
 export default function PanelEtapasV (props) {
   const etapas = LoadEtapas('http://134.209.215.193:3000/etapas/')
@@ -71,15 +51,9 @@ export default function PanelEtapasV (props) {
                 </TableRow>
                 </TableHead>
                     <TableBody>
-<<<<<<< HEAD
-                        
-                    {etapas.map(etapa=>(
-                        <TableRow key="table">
-=======
 
                     {etapas.map((etapa, id) => (
                         <TableRow key={id} >
->>>>>>> 7a130e371e37b75785df5c5f395eb6b061d750e9
                             <TableCell component="th" scope="row">
                                 {etapa.id}
                             </TableCell>
