f96c445cd5799dc61d6f70914f6263c1cc4e833f
javierVMC <javavega@espol.edu.ec>
2022-06-06 22:04:04 -0500

    themes added

---
+++ front-usuarios/src/themes/lightTheme.js
@@ -0,0 +1,10 @@
+const lightTheme = {
+  darkBlue: '#02023D',
+  lightOrange: '#F4733E',
+  darkOrange: '#E86E3C',
+  disableButtonColor: '#C7C7C7',
+  darkGray: '#878787',
+  white: '#ffffff'
+}
+
+export default lightTheme
