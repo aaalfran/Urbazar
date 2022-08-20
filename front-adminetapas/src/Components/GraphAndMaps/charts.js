d57aad3199ba114862bb79d6fa2e867e40d98934
aaalfran <aaalfran@espol.edu.ec>
2022-08-19 22:51:53 -0500

    API productos

--- front-adminetapas/src/Components/GraphAndMaps/charts.js
+++ front-adminetapas/src/Components/GraphAndMaps/charts.js
@@ -11,7 +11,7 @@ const durations = 500
 const delays2 = 80
 const durations2 = 500
 
-fetch('http://localhost:3000/productos')
+fetch(`${data.number}/productos`)
   .then(response => response.json())
   .then(data => {
     console.log(data)
